2ea09c479529969355ba59c99ebf640dc1f8d438
dumprahul <shanrahul04@gmail.com>
2025-08-30 08:04:45 +0530

    zama-fhevm init

--- packages/site/abi/FHECounterAddresses.ts
+++ packages/site/abi/FHECounterAddresses.ts
@@ -5,5 +5,5 @@
 */
 export const FHECounterAddresses = { 
   "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0x7553CB9124f974Ee475E5cE45482F90d5B6076BC", chainId: 31337, chainName: "hardhat" },
+  "31337": { address: "0x0f1e88DBbCEe8E2FCc6C97E9747413aaee557447", chainId: 31337, chainName: "hardhat" },
 };
