d8e5dbe8f17c7ff93ef2562741a3f7ac9b9a48ce
dumprahul <shanrahul04@gmail.com>
2025-08-30 23:22:41 +0530

    landing page for testing

--- packages/site/app/page.tsx
+++ packages/site/app/page.tsx
@@ -1,10 +1,10 @@
-import { FHECounterDemo } from "@/components/FHECounterDemo";
+import { ZumaEventsDemo } from "@/components/ZumaEventsDemo";
 
 export default function Home() {
   return (
     <main className="">
       <div className="flex flex-col gap-8 items-center sm:items-start w-full px-3 md:px-0">
-        <FHECounterDemo />
+        <ZumaEventsDemo />
       </div>
     </main>
   );
