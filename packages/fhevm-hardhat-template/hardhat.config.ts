d77a3c78dfd0b3c526a2793b892c54e534fd1e48
dumprahul <shanrahul04@gmail.com>
2025-08-30 16:49:19 +0530

    hardhat config due to stack too deep

--- packages/fhevm-hardhat-template/hardhat.config.ts
+++ packages/fhevm-hardhat-template/hardhat.config.ts
@@ -79,7 +79,9 @@ const config: HardhatUserConfig = {
         runs: 800,
       },
       evmVersion: "cancun",
+      viaIR: true,
     },
+
   },
   typechain: {
     outDir: "types",
--- packages/site/abi/FHECounterAddresses.ts
+++ packages/site/abi/FHECounterAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const FHECounterAddresses = { 
-  "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0x763e801c2beB1dD7cA6D18c876D25D7807ad5A72", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0x0f1e88DBbCEe8E2FCc6C97E9747413aaee557447", chainId: 31337, chainName: "hardhat" },
 };
