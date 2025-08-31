138f7cbfa2b12e211a2d4ad95f63dbf57dc6d58a
dumprahul <shanrahul04@gmail.com>
2025-08-31 09:35:57 +0530

    cleaned fhecounter template for abi and address

--- packages/site/abi/FHECounterAddresses.ts
+++
@@ -1,9 +0,0 @@
-
-/*
-  This file is auto-generated.
-  Command: 'npm run genabi'
-*/
-export const FHECounterAddresses = { 
-  "11155111": { address: "0x763e801c2beB1dD7cA6D18c876D25D7807ad5A72", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0x0f1e88DBbCEe8E2FCc6C97E9747413aaee557447", chainId: 31337, chainName: "hardhat" },
-};
